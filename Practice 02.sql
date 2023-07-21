122e3c2923ee260d72eb06ece6d4e765524dd3fd
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2023-07-21 13:34:54 +0500

    practice 02 all question completed (06 to 13)

--- Practice 02.sql
+++ Practice 02.sql
@@ -6,22 +6,20 @@ select
 FROM
     hr.employees
 where
-    salary > 12000 
+    salary > 12000;
 
 -- Question No 02
 -- Answer is 
-
 select
     last_name,
     department_id
 FROM
     hr.employees
 where
-    employee_id = 176
+    employee_id = 176;
 
 -- Question No 03
 -- Answer is 
-
 SELECT
     last_name,
     salary
@@ -55,4 +53,93 @@ FROM
 WHERE
     department_id IN (20, 50)
 ORDER BY
-    last_name ;
+    last_name;
+
+-- Question No 06
+-- Answer is 
+SELECT
+    last_name,
+    salary,
+    department_id
+FROM
+    hr.employees
+WHERE
+    salary between 5000
+    and 12000
+    and department_id IN (20, 50);
+
+-- Question No 07
+-- Answer is 
+SELECT
+    last_name,
+    hire_date
+FROM
+    hr.employees
+WHERE
+    hire_date LIKE '%94';
+
+-- Question No 08
+-- Answer is 
+SELECT
+    last_name,
+    job_id
+FROM
+    hr.employees
+WHERE
+    manager_id is null;
+
+-- Question No 09
+-- Answer is 
+SELECT
+    last_name,
+    salary,
+    COMMISSION_PCT
+FROM
+    hr.employees
+WHERE
+    COMMISSION_PCT is not null
+ORDER BY
+    salary desc,
+    COMMISSION_PCT desc;
+
+-- Question No 10
+-- Answer is 
+SELECT
+    last_name
+FROM
+    hr.employees
+WHERE
+    last_name LIKE '__a%';
+
+-- Question No 11
+-- Answer is 
+SELECT
+    last_name
+FROM
+    hr.employees
+WHERE
+    last_name LIKE '%a%'
+    and last_name LIKE '%e%';
+
+-- Question No 12
+-- Answer is 
+SELECT
+    last_name,
+    job_id,
+    salary
+FROM
+    hr.employees
+WHERE
+    job_id IN ('SA_REP', 'ST_CLERK')
+    AND salary NOT IN (2500, 3500, 7000);
+
+-- Question No 13
+-- Answer is 
+SELECT
+    last_name as "Employee",
+    salary as "Monthly Salary",
+    COMMISSION_PCT
+FROM
+    hr.employees
+WHERE
+    COMMISSION_PCT =.2;
